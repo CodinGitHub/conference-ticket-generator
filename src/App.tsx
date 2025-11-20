5f0f75d64efb8515f20d0bb5119cd0aad0c4c84f
Davichobits <davidruizdeveloper@gmail.com>
2025-11-20 08:51:17 -0500

    primera parte

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TicketFormPage } from './components/ticket-form-page/ticket-form-page'
+import { MainLayout } from './components/layouts/main-layout'
+
+function App() {
+
+  return (
+    <MainLayout>
+      <TicketFormPage />
+    </MainLayout>
+  )
+}
+
+export default App
